cc196e1e7a09953115540ac21e9f3f573937f82c
Alex J Hawk (HMS) <alha@hms.se>
2022-09-29 13:30:35 -0400

    Update existing EwonJavaMultiLoaderMain javadocs

--- src/main/java/com/hms_networks/americas/sc/javaloader/EwonJavaMultiLoaderMain.java
+++ src/main/java/com/hms_networks/americas/sc/javaloader/EwonJavaMultiLoaderMain.java
@@ -1,18 +1,36 @@
 package com.hms_networks.americas.sc.javaloader;
 
 /**
- * Example main class for Solution Center Java Starter Project. This class includes a basic main
- * method with sample code to begin with.
+ * Main class for the Ewon Java Multi-Loader application. The main method of this class loads each
+ * Java jar file within the {@link #MULTI_LOADER_CLASSPATH_FOLDER} directory of the Ewon filesystem
+ * to the Java classpath, then starts each main class/method in a new thread.
+ *
+ * <p>The documentation in this class may reference components of Java or the Ewon Flexy using
+ * abbreviated terms. These terms are not case-sensitive and are defined below:
+ *
+ * <ul>
+ *   <li>Sc - Solution Center
+ *   <li>Jvm - Java Virtual Machine
+ *   <li>Cp - Classpath
+ * </ul>
  *
  * @author HMS Networks, MU Americas Solution Center
- * @version 0.0.1
+ * @version 1.0.0
+ * @since 1.0.0
  */
 public class EwonJavaMultiLoaderMain {
 
   /**
-   * Example main method for Solution Center Java Starter Project.
+   * Main method for the Ewon Java Multi-Loader application. Each Java jar file within the {@link
+   * #MULTI_LOADER_CLASSPATH_FOLDER} directory of the Ewon filesystem is loaded to the Java
+   * classpath, then each main class/method in is started in a new thread.
    *
-   * @param args project arguments
+   * <p>Note: All log output messages are written using the {@link Logger#LOG_LEVEL_CRITICAL} log
+   * level to ensure no output is skipped due to other application interaction with the {@link
+   * Logger} class.
+   *
+   * @param args command line arguments (not used, ignored)
+   * @since 1.0.0
    */
   public static void main(String[] args) {
     // Try to output application name from Maven
